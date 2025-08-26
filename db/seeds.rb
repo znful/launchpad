1b074f198344bdba21628d0ee83f9144261f748e
David Macario <david.macariopro@gmail.com>
2025-08-26 23:06:16 +0200

    add seed & faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create! email_address: "dev@launchpad.com", password: "dev"
+
+20.times do
+  Offer.create!(
+    company_name: Faker::Company.name,
+    title: Faker::Job.title,
+    description: Faker::Lorem.paragraph(sentence_count: 10),
+    city: Faker::Address.city,
+    country: Faker::Address.country,
+    apply_link: Faker::Internet.url,
+    contract_type: Offer.contract_types.keys.sample,
+    job_type: Offer.job_types.keys.sample,
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+    user: User.first
+  )
+end

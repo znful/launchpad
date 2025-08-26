9dccf550e38b4afbe50a05eda0b82e208c399559
David Macario <david.macariopro@gmail.com>
2025-08-26 23:08:49 +0200

    setup offer model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,12 @@
 class Offer < ApplicationRecord
   belongs_to :user
+  has_rich_text :description
+
+  after_validation :geocode, if: ->(obj) { obj.country_changed? || obj.city_changed? }
+
+  validates :company_name, :title, :description, :country, :apply_link, :contract_type, :job_type, presence: true
+
+
+  enum contract_type: { full_time: 0, part_time: 1, contract: 2, internship: 3, temporary: 4, freelance: 5 }
+  enum job_type: { onsite: 0, remote: 1, hybrid: 2 }
 end

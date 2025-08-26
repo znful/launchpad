0240fe5b5c5b81f04fe2cf110c5a0b617a624f54
David Macario <david.macariopro@gmail.com>
2025-08-26 21:11:43 +0200

    switch to UUIDs

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module Launchpad
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #
+    # Use UUIDs for primary keys by default.
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_25_215353) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_26_190236) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"

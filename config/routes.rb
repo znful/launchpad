1c25474b2246a8a8ffb1b8603131a40f1617e586
David Macario <david.macariopro@gmail.com>
2025-08-28 21:47:10 +0200

    Add offers manage page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :offers
+  resources :offers do
+    collection do
+      get :manage
+    end
+  end
+
+
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

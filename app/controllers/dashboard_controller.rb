24ea6ab8e05f960766ffcd06ec0d4e9a2f8fca24
David Macario <david.macariopro@gmail.com>
2025-08-31 23:42:37 +0200

    add pagination and filters to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,24 @@
 class DashboardController < ApplicationController
-  before_action :set_offers, only: [ :index ]
+  before_action :set_filters, only: [ :index ]
 
   def index
+    @pagy, @offers = pagy(Current.user.offers, limit: @limit)
   end
 
   private
-  def set_offers
-    @pagy, @offers = pagy(Current.user.offers)
+  def set_filters
+    @limit = params[:limit] || 9
+    @sort = params[:sort] || "created_at desc"
+    @location = params[:location]
+    @range = params[:range] || 100
+    @unit = params[:unit] || "km"
+
+    if @location.blank?
+      if request.location.city.present? && !request.location.city.blank?
+        @location = request.location.city + ", " + request.location.country
+      else
+        @location = "Paris, France"
+      end
+    end
   end
 end

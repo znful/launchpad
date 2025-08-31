0bb077338ffc35b42ce0c0b27fc01f54a2add900
David Macario <david.macariopro@gmail.com>
2025-08-31 23:15:55 +0200

    Edit dashboard, add custom turbo confirm modal

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class DashboardController < ApplicationController
+  before_action :set_offers, only: [ :index ]
+
   def index
+  end
+
+  private
+  def set_offers
+    @offers = Current.user.offers
   end
 end

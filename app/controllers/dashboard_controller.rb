0c69ef19421feb1cc12748a17a68680d2f4d5a94
David Macario <86846166+znful@users.noreply.github.com>
2025-09-02 23:08:15 +0200

    Add dashboard page

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,21 @@
+class DashboardController < ApplicationController
+  before_action :set_filters, only: [ :index ]
+
+  def index
+    @q = Current.user.offers.ransack(params[:q])
+    if @location.blank?
+      @pagy, @offers = pagy(@q.result.order(@sort), limit: @limit)
+    else
+      @pagy, @offers = pagy(@q.result.near(@location, @range, units: @unit).order(@sort), limit: @limit)
+    end
+  end
+
+  private
+  def set_filters
+    @limit = params[:limit] || 9
+    @sort = params[:sort] || "created_at desc"
+    @location = params[:location]
+    @range = params[:range] || 100
+    @unit = params[:unit] || "km"
+  end
+end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 
-import "trix"
-import "@rails/actiontext"
+import "trix";
+import "@rails/actiontext";
+
+Turbo.config.forms.confirm = (message, element) => {
+  let dialog = document.getElementById("turbo-confirm");
+  let messageContainer = document.getElementById("confirm-message");
+  messageContainer.textContent = message;
+  dialog.showModal();
+
+  return new Promise((resolve) => {
+    dialog.addEventListener(
+      "close",
+      () => {
+        resolve(dialog.returnValue === "true");
+      },
+      { once: true },
+    );
+  });
+};

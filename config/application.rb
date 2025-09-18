5e7c4fa85348b5a068743a21a09afada8695b0b5
David Macario <david.macariopro@gmail.com>
2025-09-18 23:52:40 +0200

    Set up UUIDs as default

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module Launchpad
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Enable UUID primary keys by default.
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end

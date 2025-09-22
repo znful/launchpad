45c06cdb99afa1c76d177889f06c8fe54b2c5aaf
Gilguff <oharelt@gmail.com>
2025-09-22 23:21:52 +0100

    updated filters dropdown and added badge helper function

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -6,4 +6,14 @@ module OffersHelper
   def job_type_colour(job_type)
     { "on_site" => "green", "remote" => "blue", "hybrid" => "purple" }[job_type] || "gray"
   end
+
+  def contract_type_badge(key)
+    colour = contract_type_colour(key.to_s)
+    content_tag(:span, key.to_s.humanize.titleize, class: "badge px-2 py-1 badge-pill bg-#{colour}-lt text-#{colour}")
+  end
+
+  def job_type_badge(key)
+    colour = job_type_colour(key.to_s)
+    content_tag(:span, key.to_s.humanize.titleize, class: "badge px-2 py-1 badge-pill bg-#{colour}-lt text-#{colour}")
+  end
 end

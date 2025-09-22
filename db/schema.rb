9e0aa9e66da22b435026d70788de2b4b626a3284
Gilguff <oharelt@gmail.com>
2025-09-22 19:15:14 +0100

    added faker, basic offers page and new offer form modal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_20_183404) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_22_145250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -65,6 +65,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_20_183404) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "user_id", null: false
+    t.float "longitude"
+    t.float "latitude"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,19 @@
 User.find_or_create_by!(email_address: "dev@launchpad.com") do |user|
   user.password = "dev"
 end
+
+
+20.times do
+  Offer.find_or_create_by!(
+    title: Faker::Job.title,
+    company_name: Faker::Company.name,
+    address: [ Faker::Address.city, Faker::Address.country ].compact.join(", "),
+    application_link: Faker::Internet.url,
+    contract_type: Offer.contract_types.keys.sample,
+    job_type: Offer.job_types.keys.sample,
+    description: Faker::Lorem.paragraph(sentence_count: 10),
+    status: Offer.statuses.keys.sample,
+    verification_status: Offer.verification_statuses.keys.sample,
+    user: User.first
+  )
+end

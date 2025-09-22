755ec139cb379422e9b96de844c6c8b5230dc2c5
Gilguff <oharelt@gmail.com>
2025-09-22 20:45:24 +0100

    offers accordion and badges

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -1,2 +1,9 @@
 module OffersHelper
+  def contract_type_colour(contract_type)
+    { "full_time" => "green", "part_time" => "blue", "contract" => "orange", "internship" => "purple", "freelance" => "teal" }[contract_type] || "gray"
+  end
+
+  def job_type_colour(job_type)
+    { "on_site" => "green", "remote" => "blue", "hybrid" => "purple" }[job_type] || "gray"
+  end
 end

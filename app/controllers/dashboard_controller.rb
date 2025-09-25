a813f37eb06b602ed50d07180f8f32d3d78b966b
David Macario <david.macariopro@gmail.com>
2025-09-26 01:01:48 +0200

    add dashboard filters, edit stats cards

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,21 +2,29 @@ class DashboardController < ApplicationController
   before_action :set_filters, only: [ :index ]
 
   def index
-    @pagy, @offers = pagy(Current.user.offers)
+    @q = Current.user.offers.ransack(params[:q])
+
+    if @status.present?
+      @pagy, @offers = pagy(@q.result.near(@location, @range).where(status: @status).order(@sort), limit: @limit) if @location.present? && @range.present?
+      @pagy, @offers = pagy(@q.result.order(@sort).where(status: @status), limit: @limit) if @location.nil? || @location.empty?
+    else
+      @pagy, @offers = pagy(@q.result.near(@location, @range).order(@sort), limit: @limit) if @location.present? && @range.present?
+      @pagy, @offers = pagy(@q.result.order(@sort), limit: @limit) if @location.nil? || @location.empty?
+    end
+
     @view_count = @offers.views.count
     @interaction_count = @offers.interactions.count
     @application_count = @offers.applications.count
-    p Current.user.offers
     render :index, layout: "dashboard"
   end
 
   private
 
   def set_filters
-    @limit = params[:limit] || 10
+    @limit = params[:limit] || 9
     @sort = params[:sort] || "created_at desc"
-    @location = params[:location] || nil
-    @range = params[:range] || 100
-    @status = params[:status]
+    @location = params[:location]
+    @range = params[:range] || 50
+    @status = params[:status] || ""
   end
 end

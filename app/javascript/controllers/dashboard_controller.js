c227ba5dd3db05527b61c6461a1a3f9a2c447549
David Macario <david.macariopro@gmail.com>
2025-10-09 00:18:27 +0200

    change select filters to dropdowns with checkboxes

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -2,8 +2,14 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="dashboard"
 export default class extends Controller {
-  static targets = ["form"];
-  connect() {}
+  static targets = ["form", "dropdown"];
+  connect() {
+    this.dropdownTargets.forEach((dropdown) => {
+      dropdown.addEventListener("hide.bs.dropdown", () => {
+        this.submitForm();
+      });
+    });
+  }
 
   submitForm() {
     console.log("Submitting form...");

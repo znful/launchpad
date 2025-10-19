dd2da7077c3307699b905f06c6320ce1fc68775e
David Macario <david.macariopro@gmail.com>
2025-10-19 17:25:45 +0200

    allow unauthenticated access to landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  allow_unauthenticated_access
+
   def index
     @q = Offer.ransack(params[:q])
     @offers = Offer.all.limit(3).order(created_at: :desc)

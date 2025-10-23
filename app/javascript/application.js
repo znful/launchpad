e50ecdd5d73caa19ac37b0972959c50620e86743
David Macario <david.macariopro@gmail.com>
2025-10-23 03:54:05 +0200

    added custom confirm dialog

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,35 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 
-import "trix"
-import "@rails/actiontext"
+import "trix";
+import "@rails/actiontext";
+
+Turbo.config.forms.confirm = (message, _) => {
+  let dialog = document.getElementById("turbo-confirm");
+  let messageContainer = dialog.querySelector("#confirm-message");
+  const closeButton = dialog.querySelector(".btn-close");
+
+  closeButton.addEventListener(
+    "click",
+    () => {
+      dialog.close("false");
+    },
+    { once: true },
+  );
+
+  messageContainer.textContent = message;
+  dialog.classList.remove("d-none");
+  dialog.showModal();
+
+  return new Promise((resolve) => {
+    dialog.addEventListener(
+      "close",
+      () => {
+        dialog.classList.add("d-none");
+        resolve(dialog.returnValue === "true");
+      },
+      { once: true },
+    );
+  });
+};

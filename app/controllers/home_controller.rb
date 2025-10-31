2abc4cef63e6d7ee534fb96b68dd689648976fa0
David Macario <david.macariopro@gmail.com>
2025-10-31 01:13:52 +0100

    fixed dashboard#index offer cards

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
 
   def index
     @q = Offer.ransack(params[:q])
-    @offers = Offer.all.limit(3).order(created_at: :desc)
+    @offers = Offer.all.limit(3).order(created_at: :desc).where(status: :published)
   end
 end

6f85361b6fdb75548f493a88624dff74967b085c
David Macario <david.macariopro@gmail.com>
2025-11-01 21:19:28 +0100

    updated confirm modal to be paramertised

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,12 +5,28 @@ import "controllers";
 import "trix";
 import "@rails/actiontext";
 
-Turbo.config.forms.confirm = (message, _) => {
+Turbo.config.forms.confirm = (message, element) => {
   const dialog = document.getElementById("turbo-confirm");
   const messageContainer = dialog.querySelector("#confirm-message");
-  const confirmButton = dialog.querySelector("#confirm-button");
   const body = document.querySelector("body");
+  const modalStatusBar = dialog.querySelector(".modal-status");
+  const confirmButton = document.getElementById("confirm-button");
+  const modalTitleContainer = document.getElementById("confirm-title");
+
   const backdrop = document.createElement("div");
+
+  const modalVariant = element.getAttribute("data-variant") || "primary";
+  const modalTitle = element.getAttribute("data-title") || "Please Confirm";
+
+  if (modalVariant) {
+    modalStatusBar.classList.add(`bg-${modalVariant}`);
+    confirmButton.classList.add(`btn-${modalVariant}`);
+  }
+
+  if (modalTitle) {
+    modalTitleContainer.innerText = modalTitle;
+  }
+
   body.style.overflow = "hidden";
   backdrop.className = "modal-backdrop fade show";
 
@@ -28,6 +44,7 @@ Turbo.config.forms.confirm = (message, _) => {
         dialog.classList.add("d-none");
         body.classList.remove("modal-open");
         body.removeChild(backdrop);
+        body.style.overflow = "auto";
         resolve(dialog.returnValue === "true");
       },
       { once: true },

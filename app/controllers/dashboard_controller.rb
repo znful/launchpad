ba5a0fd83b8259db57ef7a8293c64df9af2e140b
David Macario <david.macariopro@gmail.com>
2025-11-02 18:58:54 +0100

    fix dashboard filters, added limit filter

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,30 +2,30 @@ class DashboardController < ApplicationController
   before_action :set_filters, only: [ :index ]
 
   def index
-    @q = Current.user.offers.ransack(params[:q])
+    @q = Offer.ransack(params[:q])
+    @pagy, @offers = pagy(@q.result(distinct: true).order(created_at: :desc), limit: @limit)
     @offer = Offer.new
 
-    if @status.present?
-      @pagy, @offers = pagy(@q.result.near(@location, @range).where(status: @status).order(@sort), limit: @limit) if @location.present? && @range.present?
-      @pagy, @offers = pagy(@q.result.order(@sort).where(status: @status), limit: @limit) if @location.nil? || @location.empty?
-    else
-      @pagy, @offers = pagy(@q.result.near(@location, @range).order(@sort), limit: @limit) if @location.present? && @range.present?
-      @pagy, @offers = pagy(@q.result.order(@sort), limit: @limit) if @location.nil? || @location.empty?
+    if @location.present? && @distance.present?
+      location = params[:q][:location]
+      distance = params[:q][:distance].to_f
+
+      @pagy, @offers = pagy(@offers.near(location, distance, units: :km), limit: @limit)
     end
 
-    @view_count = @offers.views.count
-    @interaction_count = @offers.interactions.count
-    @application_count = @offers.applications.count
+    @view_count = Current.user.offers.views.count
+    @interaction_count = Current.user.offers.interactions.count
+    @application_count = Current.user.offers.applications.count
     render :index, layout: "dashboard"
   end
 
   private
 
   def set_filters
-    @limit = params[:limit] || 9
+    @limit = params[:limit] || 10
     @sort = params[:sort] || "created_at desc"
-    @location = params[:location]
-    @range = params[:range] || 50
+    @location = params.dig(:q, :location)
+    @distance = params.dig(:q, :distance).to_f || 50
     @status = params[:status] || ""
   end
 end

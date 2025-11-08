816bbe27cb5bcdee52c79bad5f06d55259e26f42
David Macario <david.macariopro@gmail.com>
2025-11-08 14:44:34 +0100

    add admin_dashboard controller tests

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -6,4 +6,16 @@ class Admin::DashboardControllerTest < ActionDispatch::IntegrationTest
     get admin_dashboard_url
     assert_response :success
   end
+
+  test "signed in user without admin rights should be redirected from index" do
+    sign_in users(:two)
+    get admin_dashboard_url
+    assert_redirected_to offers_url
+    assert_equal "You are not authorized to access this page.", flash[:alert]
+  end
+
+  test "unauthenticated user should be redirected from index" do
+    get admin_dashboard_url
+    assert_redirected_to new_session_url
+  end
 end

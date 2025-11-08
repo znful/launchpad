83db313686a21b89ae064bc284266b8a8a7d3d98
David Macario <david.macariopro@gmail.com>
2025-11-08 14:42:12 +0100

    add admin field to fixtures, fix admin_dashboard test

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -1,8 +1,9 @@
 require "test_helper"
 
 class Admin::DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get admin_dashboard_index_url
+  test "signed in user with admin rights should get index" do
+    sign_in users(:one)
+    get admin_dashboard_url
     assert_response :success
   end
 end
